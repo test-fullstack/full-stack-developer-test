06d7fa0e2dd763d9ae0fdd30af9d407e2fb79172
test-fullstack <244414867+test-fullstack@users.noreply.github.com>
2025-11-19 14:28:28 +0100

    Update the version of the libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.spring") version "2.3.0-Beta2"
     id("org.springframework.boot") version "3.5.7"
     id("io.spring.dependency-management") version "1.1.7"
 }
